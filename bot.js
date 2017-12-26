da44feeb58fa085a72ad90a9c296b42b6162f83b
NoNames <UvHyMew@gmail.com>
2017-12-26 18:52:28 +0700

    Fix MySQL Connection

--- bot.js
+++ bot.js
@@ -25,17 +25,36 @@ fs.readdir("./cmds/", (err, files) => {
 	})
 });
 
-var con = mysql.createConnection({
-	host: process.env.dbHost,
-	user: process.env.dbUser,
-	password: process.env.dbPass,
-	database: process.env.db
-});
+var con;
 
-con.connect(err => {
-	if(err) throw err;
-	console.log("Database Connected!");
-});
+function mysqlService() {
+	con = mysql.createConnection({
+		host: process.env.dbHost,
+		user: process.env.dbUser,
+		password: process.env.dbPass,
+		database: process.env.db
+	});
+
+	con.connect(err => {
+		if(err) {
+			console.log('Error When Connecting To DB:', err);
+			setTimeout(handleDisconnect, 2000);
+		}
+		console.log("Database Connected!");
+	});
+
+	connection.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+mysqlService();
+
 
 bot.on('ready', async () => {
     console.log(`Bot Is Ready! ${bot.user.username}`);
